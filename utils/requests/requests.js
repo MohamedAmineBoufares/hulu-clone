380915c166c39b5ab9662a76d2c919aca0105e68
Mohamed Amine <medamineboufares@gmail.com>
2022-09-17 17:49:01 +0100

    added fade effect + hide scrollbar

--- utils/requests/requests.js
+++ utils/requests/requests.js
@@ -1,6 +1,6 @@
 const API_KEY = process.env.API_KEY;
 
-const config = {
+const requests = {
   fetchTrending: {
     title: "Trending",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -43,4 +43,4 @@ const config = {
   },
 };
 
-export default config;
+export default requests;
